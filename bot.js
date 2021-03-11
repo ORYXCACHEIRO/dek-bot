a6d95d5ec55dc0bf8f00d46ef805f0d1506d0f72
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-11 20:37:21 +0000

    Erro token

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 
 var client = new discord.Client();
 
-const token = "ODE5NjU4Mjc1MDAzMDM5NzU1.YEp0QQ.Jagcb3mbIZQDB4_bk9Tan06JyE8";
+client.login("ODE5NjU4Mjc1MDAzMDM5NzU1.YEp0QQ.Jagcb3mbIZQDB4_bk9Tan06JyE8");
 
 client.on("ready", () => {
     console.log("ready");
