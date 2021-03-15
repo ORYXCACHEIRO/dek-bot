5237917798ac918066704e3d0e2b71a7dffd0f00
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-15 00:58:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ client.login("ODE5NjU4Mjc1MDAzMDM5NzU1.YEp0QQ.Jagcb3mbIZQDB4_bk9Tan06JyE8");
 client.on("ready", () => {
     console.log("Bot ready and online (and updated as well)");
     client.user.setActivity ("Being a good bot");
-});
 
-client.channels.add("Merda");
+    guild.channels.create('LOR Deck BOT', { reason: 'Needed a cool new channel' }).then(console.log).catch(console.error);
+});
           
 client.on("message", (message) => {
 
