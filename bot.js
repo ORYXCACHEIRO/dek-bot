20aa760382f8582d0373d18771ed9f9884b20838
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-15 03:31:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,36 +2,32 @@ const discord = require('discord.js');
 
 var client = new discord.Client();
 
-var channel_Id = "";
-
 client.login("ODE5NjU4Mjc1MDAzMDM5NzU1.YEp0QQ.Jagcb3mbIZQDB4_bk9Tan06JyE8");
-
-var c = new discord.GuildChannelManager();
-
-//const embeded = new discord.MessageEmbed().setColor("#fcc603");
 
 client.on("ready", () => {
     console.log("Bot ready and online (and updated as well)");
-    client.user.setActivity ("Being a good bot"); 
-
-    if(channel_Id==""){
-        c.channels.create("LOR DECKS").then(ch => {
-            channel_Id = ch.id;
-        });
-    }
-
+    client.user.setActivity ("Being a good bot");                                                                                                                                                                                                                                                                                                                                                         
 });
-
-
           
 client.on("message", (message) => {
-   
+
+    var channel_Id = "";
+
     if(message.author.bot) return; 
 
-    const prefix = "ld!";
+    const prefix = "ld!"
+
+    const embeded = new discord.MessageEmbed().setColor("#fcc603");
 
     if(message.content==prefix+"setupbot"){
-        
+        if(channel_Id!=""){
+            message.guild.channels.create("LOR DECKS").then(ch => {
+                channel_Id = ch.id;
+            });
+        }
+       else {
+           message.reply("The bot was already setup");
+       }
     }
 
 });
