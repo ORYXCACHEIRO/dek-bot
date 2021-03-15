47c6bb972e2df26b586fffd79ce84de431231fc2
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-15 02:33:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,25 +4,21 @@ var client = new discord.Client();
 
 client.login("ODE5NjU4Mjc1MDAzMDM5NzU1.YEp0QQ.Jagcb3mbIZQDB4_bk9Tan06JyE8");
 
+var channel_Id = "";
+
 client.on("ready", () => {
     console.log("Bot ready and online (and updated as well)");
-    client.user.setActivity ("Being a good bot");
-
-    if(client.channels.fetch("LOR DECKS")!=true){
-        console.log("noice");
-    }
+    client.user.setActivity ("Being a good bot");                                                                                                                                                                                                                                                                                                                                                         
 });
           
 client.on("message", (message) => {
 
     if(message.author.bot) return; 
 
-    msg = message.content.toLowerCase();
-
-    if(message.content=="hello"){
-        message.reply("Aqui tens os comandos");
-    }
-    
-    
+    if(message.content=="Setup Bot"){
+       message.guild.channels.create("LOR DECKS").then(ch => {
+           channel_Id = ch.id; 
+       });
+    }   
 
 });
