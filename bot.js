9b119e799b2feaaa62ef407bea215bc8119d5f1b
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-16 22:50:50 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,10 +57,14 @@ client.on("message", (message) => {
         });
 
         if(found!=true){
-            message.guild.channels.create("lor decks").then(ch => {
-                channel_Id = ch.id;
-            });
-            message.reply("Bot is setup");
+            message.guild.channels.create("lor decks");
+            message.channel.send(
+                embeded.setTitle("Bot Setup")
+                .setDescription("The bot was succefully setup " + message.guild.channels.cache.get(lorDeckChannelId()).toString())
+                .setThumbnail("https://static.wikia.nocookie.net/leagueoflegends/images/2/2c/Legends_of_Runeterra_icon.png/revision/latest?cb=20191020214918")
+                .setFooter("If you neeed help use ld!help for more commands")
+                .setTimestamp()
+            );
         }
 
     }
