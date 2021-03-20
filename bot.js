994713da399f10920c288fff0dfea365ffe71002
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-20 23:13:37 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -619,7 +619,7 @@ client.on("message", (message) => {
 
     function errorFindingDeck(){
         message.channel.send(
-            embeded.setTitle("Error")
+             embeded.setTitle("Error")
             .setDescription("This deck doesn't exist or you dont have any decks created")
             .setThumbnail("https://static.wikia.nocookie.net/leagueoflegends/images/2/2c/Legends_of_Runeterra_icon.png/revision/latest?cb=20191020214918")
             .setFooter("If you neeed help use ld!help for more commands")
