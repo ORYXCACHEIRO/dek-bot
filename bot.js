09db7557c64c8e2590c8550d3bfcd84ee86d9aba
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-03-20 15:37:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ client.on("message", (message) => {
     else if(msg.startsWith(prefix+"updeck")){
         if(message.channel.name==nomeCanal){
 
-            const deck = DeckEncoder.decode(message.content.replace(prefix+"updeck",'').substring(1));
+            const deck = DeckEncoder.decode(message.content.replace(prefix+"updeck",''));
 
             if(deck.length>0){
 
@@ -356,7 +356,7 @@ client.on("message", (message) => {
 
     else if(msg.startsWith(prefix+"deck")){
         if(message.channel.name==nomeCanal){
-            const deck = DeckEncoder.decode(message.content.replace(prefix+"deck",'').substring(1));
+            const deck = DeckEncoder.decode(message.content.replace(prefix+"deck",''));
 
             if(deck.length>0){
                 var printDeck = new Array;
